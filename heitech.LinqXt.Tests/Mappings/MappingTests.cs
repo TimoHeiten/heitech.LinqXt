ea80d4da0b0d919373ae358ef79ae9bdd7dbe66f
tih <timoheiten@googlemail.com>
2018-03-11 17:19:33 +0100

    First Implementation of TrySubsitute_X for Dictionary/Maps

---
+++ heitech.LinqXt.Tests/Mappings/MappingTests.cs
@@ -0,0 +1,49 @@
+﻿using heitech.LinqXt.Mappings;
+using heitech.LinqXt.Tests.Enumerables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace heitech.LinqXt.Tests.Mappings
+{
+    [TestClass]
+    public class MappingTests : EnumerableTestBase
+    {
+        private readonly string newKey = "newKey";
+        private readonly string newValue = "newValue";
+
+        // MappingXt_
+        [TestMethod]
+        public void MappingXt_TrySubstituteKeyReturnsFalseIfKeyDoesNotExist()
+            => Assert.IsFalse(Dictionary.TrySubstituteKey("noKey", newKey));
+
+        [TestMethod]
+        public void MappingXt_TrySubstituteKeyReturnsTrueIfKeyDoesExistAndWasExchanged()
+        {
+            Assert.IsTrue(Dictionary.TrySubstituteKey("key_1", newKey));
+            Assert.IsTrue(Dictionary.ContainsKey(newKey));
+        }
+
+        [TestMethod]
+        public void MappingXt_TrySubstituteValueReturnsFalseIfKeyDoesNotExist()
+            => Assert.IsFalse(Dictionary.TrySubstituteValue("abcaffeschnee", newValue));
+
+        [TestMethod]
+        public void MappingXt_TrySubstituteValueReturnsTrueIfKeyDoesExistAndWasExchanged()
+        {
+            Assert.IsTrue(Dictionary.TrySubstituteValue("a", newValue));
+            Assert.IsTrue(Dictionary.Values.Any(x => x == newValue));
+        }
+
+        [TestMethod]
+        public void MappingXt_TrySubstituteKeyValueReturnsFalseIfKeyDoesNotExist()
+            => Assert.IsFalse(Dictionary.TrySubstituteKeyValuePair(("_", "v"), ("--", "vv")));
+
+        [TestMethod]
+        public void MappingXt_TrySubstituteKeyValueReturnsTrueIfKeyValuePairExistsAsGiven()
+        {
+            Assert.IsTrue(Dictionary.TrySubstituteKeyValuePair(("key_1", "a"), (newKey, newValue)));
+            Assert.IsTrue(Dictionary.ContainsKey(newKey));
+            Assert.IsTrue(Dictionary.Any(x => x.Value == newValue));
+        }
+    }
+}
---
+++ heitech.LinqXt/Mappings/MappingXt.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace heitech.LinqXt.Mappings
+{
+    public static class MappingXt
+    {
+        public static bool TrySubstituteKey<K, V>(this IDictionary<K, V> dictionary, K key, K sndKey)
+        {
+            if (dictionary.TryGetValue(key, out V val))
+            {
+                dictionary.Remove(key);
+                dictionary.Add(sndKey, val);
+                return true;
+            }
+            else
+                return false;
+        }
+        /// <summary>
+        /// Uses default Equals method for value
+        /// </summary>
+        public static bool TrySubstituteKeyValuePair<K, V>(this IDictionary<K, V> dictionary, 
+            (K key, V val) kv1, (K key, V val) kv2)
+        {
+            if (dictionary.TryGetValue(kv1.key, out V val))
+            {
+                if (val.Equals(kv1.val))
+                {
+                    dictionary.Remove(kv1.key);
+                    dictionary.Add(kv2.key, kv2.val);
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Uses default Equals method for value
+        /// </summary>
+        public static bool TrySubstituteValue<K, V>(this IDictionary<K, V> dictionary, V value, V sndVal)
+        {
+            var k_v = dictionary.FirstOrDefault(x => x.Value.Equals(value));
+            if (!k_v.Equals(default(KeyValuePair<K, V>)))
+            {
+                dictionary[k_v.Key] = sndVal;
+                return true;
+            }
+            return false;
+        }
+
+        public static (K key, V value)[] ToTupleArray<K, V>(this IDictionary<K, V> dictionary)
+        {
+            var list = new List<(K k, V v)>();
+            foreach (var item in dictionary)
+                list.Add((item.Key, item.Value));
+
+            return list.ToArray();
+        }
+    }
+}
